adeab462ca02071524816d0ecad031d18ce2c996
softdev629 <mmeliukh71@gmail.com>
2024-03-22 19:24:28 -0700

    add header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 /* ========== Navigation =========== */
+const navList = document.querySelector(".nav-list");
+
+document.querySelector(".hamburger").onclick = () => {
+  navList.classList.add("show");
+};
+
+document.querySelector(".close").onclick = () => {
+  navList.classList.remove("show");
+};
 
 /* ========== User Form =========== */
